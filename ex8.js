a44a62f0dc1053cb39471a414dbeb1634e4ea2a5
Hanbyul Jin <bhjvv57@gmail.com>
2016-11-23 22:04:37 +0900

    ex8 modified

--- ex8.js
+++ ex8.js
@@ -4,9 +4,9 @@ var url = process.argv[2];
 
 http.get(url, function(res){
         res.pipe(bl(function (err, data){
+                    if(err) return console.error(err);
                     data = data.toString();
                     console.log(data.length);
                     console.log(data);
                     }))
-        res.on("error", console.error);
-        }).on("error", console.error);
+        })
