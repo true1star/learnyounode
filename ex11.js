38afe4a9ada0ecfdf83087f22b794da16681c0ed
Hanbyul Jin <bhjvv57@gmail.com>
2016-11-24 16:07:10 +0900

    ex 11 modified

--- ex11.js
+++ ex11.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (req, res){
+          res.writeHead(200, {'content-type': 'text/plain'})
+          
           var stream = fs.createReadStream(process.argv[3]);
           stream.pipe(res)
         });
